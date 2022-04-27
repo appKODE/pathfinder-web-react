857625ae1dca2b2c2f1dde9fa82daf6ed041c844
Sophie Milash <sophie.milash@yandex.ru>
2022-04-27 13:06:54 +0200

    style(upload-spec): change styles for UploadSpec input

--- src/shared/ui/atoms/index.ts
+++ src/shared/ui/atoms/index.ts
@@ -2,3 +2,4 @@ export { Button } from './button';
 export { PanelButton } from './panel-button';
 export { CloseIcon } from './close-icon';
 export { UploadIcon } from './upload-icon';
+export { AddIcon } from './add-icon';
--- src/shared/ui/molecules/upload-spec/upload-spec.tsx
+++ src/shared/ui/molecules/upload-spec/upload-spec.tsx
@@ -1,22 +1,74 @@
 import React, { ChangeEvent } from 'react';
 import styled from 'styled-components';
 
+import { AddIcon } from '../../atoms';
+
+const Text = styled.span`
+  position: relative;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 14px;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.colors.main.dark.normal};
+
+  svg {
+    transform: translateX(-4px);
+    transition: 0.4s ease;
+  }
+`;
+
 const Wrapper = styled.label`
+  position: relative;
   display: inline-block;
   margin: 8px;
-  padding: 8px 16px;
-  color: ${({ theme }) => theme.colors.digital.blue.normal};
-  border-radius: 4px;
-  transition: 0.2s linear;
+  padding: 12px 14px;
+  padding-left: 18px;
+  transition: 0.3s ease;
+  user-select: none;
   cursor: pointer;
 
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    border-radius: 40px;
+    background-color: ${({ theme }) => theme.colors.digital.blue.translucent};
+    transition: 0.4s ease;
+    z-index: 0;
+  }
+
+  &:focus-within,
   &:hover {
-    background: ${({ theme }) => theme.colors.decorative.light.normal};
+    &::before {
+      width: 100%;
+      background-color: ${({ theme }) => theme.colors.digital.blue.normal};
+      opacity: 0.7;
+    }
+
+    ${Text} {
+      svg {
+        transform: translateX(0);
+      }
+    }
+  }
+
+  &:active {
+    transform: scale(0.96);
   }
 `;
 
 const HiddenInput = styled.input`
-  display: none;
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
 `;
 
 type Props = {
@@ -41,7 +93,10 @@ export const UploadSpec = ({ onLoad }: Props) => {
 
   return (
     <Wrapper htmlFor="upload-spec">
-      <span>Upload specification</span>
+      <Text>
+        Upload specification
+        <AddIcon size={14} />
+      </Text>
       <HiddenInput
         id="upload-spec"
         type="file"
