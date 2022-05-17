6b37274efdd9ec783c4433efe89ba0a37d96169e
Евгений Жданов <ze@kode.ru>
2022-05-17 15:32:06 +0000

    264/feature(search-input): added input for quick path searching

--- src/shared/ui/atoms/index.ts
+++ src/shared/ui/atoms/index.ts
@@ -2,3 +2,4 @@ export { Button } from './button';
 export { PanelButton } from './panel-button';
 export { RadioInput } from './radio-input';
 export { ScrollWrapper } from './scroll-wrapper';
+export { Method } from './method';
